5269e42b8cd2b0a2aa73b1ec267992d4ae5a11eb
ken88ling <ken88ling@gmail.com>
2021-04-06 17:30:12 +0800

    init

---
+++ jetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
